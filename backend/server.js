3390aa3d2d69ea100584bf182d71201ba20b0446
suman <thamansupa@gmail.com>
2025-03-15 14:05:58 +1100

    second commit

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const app = express();
 
 app.use(cors());
 const PORT = process.env.PORT||5000 ;
-//const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
@@ -37,14 +37,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
-/*
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
-}*/
+}
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
